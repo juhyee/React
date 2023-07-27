db77084655dec5165819df53365508a27ea25532
juhyee <vlel3657@naver.com>
2023-07-27 17:31:52 +0900

    feat: 20230727 Props & State

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import Composition from './components/7-27.Props/Composition';
-import Extraction from './components/7-27.Props/Extraction/Extraction';
+import Composition from './components/2-4.Props/Composition';
+import Extraction from './components/2-4.Props/Extraction/Extraction';
+import ClassComponents from './components/2-5.State/ClassComponents';
+import FunctionalComponent from './components/2-5.State/FunctionalComponent';
 
 function App() {
   return (
     <div className="App">
+      <FunctionalComponent />
+      <ClassComponents />
       <Extraction />
       <Composition />
     </div>
