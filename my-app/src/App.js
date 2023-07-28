64642a3d473fa499e955e4b96f25528580cb5a7f
juhyee <vlel3657@naver.com>
2023-07-28 14:44:21 +0900

    feat: 20230728 컴포넌트생명주기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 import Composition from './components/2-4.Props/Composition';
 import Extraction from './components/2-4.Props/Extraction/Extraction';
 import ClassComponents from './components/2-5.State/ClassComponents';
 import FunctionalComponent from './components/2-5.State/FunctionalComponent';
+import ClassComponents2 from './components/2-6.LifeCtcle/ClassComponent';
 
 function App() {
   return (
     <div className="App">
-      <FunctionalComponent />
-      <ClassComponents />
-      <Extraction />
-      <Composition />
+      <ClassComponents2 />
+
     </div>
   );
 }
