f2de6c03a9568aab1e21ec0d7861a02405c35c91
juhyee <vlel3657@naver.com>
2023-09-26 16:25:37 +0900

    feat: 20230926 begin-react

--- moz-todo-react/src/index.js
+++ moz-todo-react/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App subject='Clarice'/>, document.getElementById("root"));
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Repeat", completed: false },
+];
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+);
