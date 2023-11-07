1ab9223ced4983e4e082e89f44d8bd4578195e20
juhyee <vlel3657@naver.com>
2023-11-07 15:06:18 +0900

    feat: product-list

--- product-list/src/App.js
+++ product-list/src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header/Header.js";
 import PrdCard from "./components/PrdCard/PrdCard.js";
 
 function App() {
+
   return (
   <>
     <Header />
--- product-list/src/components/PrdCard/PrdCard.js
+++ product-list/src/components/PrdCard/PrdCard.js
@@ -1,29 +1,39 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import uuid from 'react-uuid'
 import '../PrdCard/PrdCard.scss'
 import dummy from "../../data/store.json"
 
+
 function PrdCard() {
-    function Tag(){
-        
-    }
+
+    useEffect(() => {
+        let priceNum = document.querySelectorAll('.price')
+        priceNum.forEach((itme, _) => (
+            itme.innerText = itme.innerText.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        ))
+    })
     return (
         <>
             <div className='prdCard'>
-                {dummy.products.map((item, idx) => (
-                    <div className='prdCard-item'>
+                {dummy.products.map((item, _) => (
+                    <div className='prdCard-item' key={uuid()}>
                         <a href='/' className='prdCard-item__overlay'>
-                            <div className='prdCard-item__img' key={item.id} >
-                                {/* <img src={PrdImg} alt='test' /> */}
+                            <div className='prdCard-item__img' >
+                                <img src={item.img} alt='test' />
                             </div>
                             <div className="prdCard-item__info">
                                 <span className="prdCard-item__brand">{item.brand}</span>
                                 <p className="prdCard-item__name">{item.title}</p>
                                 <div className="prdCard-item__star">
-                                    <div className={`star star${item.star}`}><span className="blind"></span></div>
+                                    <div className="star" style={{ width: item.star * 2 + '%' }}><span className="blind"></span></div>
                                 </div>
                                 <p className="prdCard-item__price"><span className="sale">{item.sale}%</span> <span className="price">{item.price}</span>원</p>
                                 <div className="prdCard-item__tag">
-                                <span className={`tag ${dummy.products[idx].tag[idx]}`}></span>
+                                    {
+                                        item.tag.map((item) => (
+                                           <span className={`tag ${item}`} key={uuid()}></span>
+                                        ))
+                                    }
                                 </div>
                             </div>
 
