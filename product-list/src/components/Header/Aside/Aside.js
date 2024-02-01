8f354d799034b30d2f5815ee34f0dee231330764
juhyee <vlel3657@naver.com>
2024-02-01 11:04:47 +0900

    feat: product-list aside 카테고리 추가

--- product-list/src/components/Header/Aside/Aside.js
+++ product-list/src/components/Header/Aside/Aside.js
@@ -5,40 +5,48 @@ import AsideItem from './AsideItem';
 import uuid from 'react-uuid'
 
 function Aside() {
-  
+
   // 임시 쇼파 영역 on 
   const [activeIndex, setActiveIndex] = useState(4)
-  
+
   const toggleActive = (index) => {
     activeIndex === index ? setActiveIndex(undefined) : setActiveIndex(index);
   }
-  
 
-  
+  dummy.aside.map((item, _) => {
+    console.log(item.children)
+  })
+
   return (
     <>
-      <div className='aside'>
+      <div className='aside__wrap'>
         {
-          dummy.aside.map((item, _) => (
-            <h3 className='aside-title' key={uuid()}>{item.title}</h3>
-          ))
+          dummy.aside.map((item, idx) => {
+            return <div className='aside'>
+              {
+                !idx ? <h3 className='aside-title' key={uuid()}>{item.title}</h3>
+                  : <h3 className='aside-title' key={uuid()}><a href={item.url}>{item.title}</a></h3>
+              }
+              {
+                !idx ? <ul className='aside-list'>
+                  {
+                    item.children.map((item, idx) => (
+                      <AsideItem
+                        key={uuid()}
+                        url={item.url}
+                        title={item.title}
+                        data={item}
+                        isActive={activeIndex === idx}
+                        onShow={() => toggleActive(idx)}
+                      />
+                    ))
+
+                  }
+                </ul>
+                  : ""}
+            </div>
+          })
         }
-        <ul className='aside-list'>
-          {
-            dummy.aside.map((item, _) => (
-              item.children.map((item, idx) => (
-                <AsideItem 
-                  key={uuid()}
-                  url={item.url}
-                  title={item.title}
-                  data={item}
-                  isActive={activeIndex === idx}
-                  onShow={() => toggleActive(idx)}
-                />
-              ))
-            ))
-          }
-        </ul>
       </div>
     </>
   )
