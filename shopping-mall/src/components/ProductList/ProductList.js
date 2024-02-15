7e0551abda912471567d874150619e1398d912db
juhyee <vlel3657@naver.com>
2024-02-15 15:54:24 +0900

    feat: sliderBanner 추가

--- shopping-mall/src/components/ProductList/ProductList.js
+++ shopping-mall/src/components/ProductList/ProductList.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './ProductList.scss'
-
+import SliderBanner from 'components/SliderBanner/SliderBanner'
 
 function ProductList() {
   return (
     <>
         <div className='contents' id='contents'>
-            
+        <SliderBanner />
+        
         </div>
     </>
   )
--- shopping-mall/src/components/SliderBanner/SliderBanner.js
+++ shopping-mall/src/components/SliderBanner/SliderBanner.js
@@ -7,6 +7,7 @@ import 'swiper/css';
 function SliderBanner() {
   return (
     <>
+      
       <div className='prdBanner'>
         <Swiper
           className='prdBanner-swiper'
@@ -19,7 +20,10 @@ function SliderBanner() {
           loop={true}
           navigation={true}
         >
-          
+          <SwiperSlide> <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/store/banners/category_banners/170766228811235400.png' alt='해당 이미지의 저작권은 오늘의집 어플에 있습니다. '/></SwiperSlide>
+          <SwiperSlide> <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/store/banners/category_banners/170774989144484772.png' alt='해당 이미지의 저작권은 오늘의집 어플에 있습니다. '/></SwiperSlide>
+          <SwiperSlide><img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/store/banners/category_banners/170779846174054654.png' alt='해당 이미지의 저작권은 오늘의집 어플에 있습니다. '/></SwiperSlide>
+          <SwiperSlide> <img src='https://image.ohou.se/i/bucketplace-v2-development/uploads/store/banners/category_banners/170774948841269579.png' alt='해당 이미지의 저작권은 오늘의집 어플에 있습니다. '/></SwiperSlide>
         </Swiper>
       </div>
     </>
